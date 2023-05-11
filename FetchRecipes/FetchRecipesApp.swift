73bd6b2bbc8d9e8a92254a7927e046e470cc9d7e
mvertude <mavdalit@gmail.com>
2023-05-11 10:06:21 -0700

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Maveric Vertudez on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
