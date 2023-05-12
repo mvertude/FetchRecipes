1a81cef20fd41efdf13eb126ab7ac985f0c8c350
mvertude <mavdalit@gmail.com>
2023-05-12 10:48:10 -0700

    Updates DetailView to display data correctly. This code resolves the issue where duplicate ingredients were not displaying properly by mapping ingredients and their measurements to a unique string (relative to their Recipe struct). Since the website where we are actually acquiring all of this data will display two separate values for an ingredient like "Butter" we will do the same in this implementation.

--- FetchRecipes/DetailView.swift
+++ FetchRecipes/DetailView.swift
@@ -11,6 +11,7 @@ struct Recipe: Codable {
     var instructions: String = ""
     var ingredients: [String] = []
     var measurements: [String] = []
+    var hm: [String:String] = [:]
 }
 
 struct DetailView: View {
@@ -29,10 +30,15 @@ struct DetailView: View {
                 }
                 Text(dessert.strMeal)
                 Text(recipe.instructions)
-                ForEach(Array(zip(recipe.ingredients, recipe.measurements)), id: \.0) { item in
+//                ForEach(Array(zip(recipe.ingredients, recipe.measurements)), id: \.0) { item in
+//                    HStack {
+//                        Text("\(item.0)")
+//                        Text(item.1)
+//                    }
+//                }
+                ForEach(Array(recipe.hm.keys), id: \.self) { key in
                     HStack {
-                        Text("\(item.0)")
-                        Text(item.1)
+                        Text(recipe.hm[key]!)
                     }
                 }
             }
@@ -53,6 +59,9 @@ struct DetailView: View {
                 recipe.instructions = dict["strInstructions"]!!
                 recipe.ingredients = dict.filter({$0.key.hasPrefix("strIngredient") && $0.value != "" && $0.value != nil}).sorted(by: { Int($0.key.dropFirst(13))! < Int($1.key.dropFirst(13))!}).map({$0.value!})
                 recipe.measurements = dict.filter({$0.key.hasPrefix("strMeasure") && $0.value != " " && $0.value != nil}).sorted(by: { Int($0.key.dropFirst(10))! < Int($1.key.dropFirst(10))!}).map({$0.value!})
+                for i in 0..<recipe.ingredients.count {
+                    recipe.hm["item" + String(i + 1)] = recipe.ingredients[i] + ", " + recipe.measurements[i]
+                }
             }
         } catch {
             print(error)
