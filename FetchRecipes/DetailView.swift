115f922eb8b8f3223d8caaf4078792dadaa7a5bf
mvertude <mavdalit@gmail.com>
2023-05-12 11:11:40 -0700

    Updates recipe instructions to display as a numbered list delimited by periods

--- FetchRecipes/DetailView.swift
+++ FetchRecipes/DetailView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct Recipe: Codable {
-    var instructions: String = ""
+    var instructions: [String] = []
     var ingredients: [String] = []
     var measurements: [String] = []
     var hm: [String:String] = [:]
@@ -29,7 +29,13 @@ struct DetailView: View {
                     ProgressView()
                 }
                 Text(dessert.strMeal)
-                Text(recipe.instructions)
+//                Text(recipe.instructions)
+                ForEach(Array(recipe.instructions.enumerated()), id: \.offset) { i, content in
+                    if content != "" {
+                        Text(String(i + 1) + ". " + content + ".")
+                    }
+                }
+//                Text(recipe.instructions)
 //                ForEach(Array(zip(recipe.ingredients, recipe.measurements)), id: \.0) { item in
 //                    HStack {
 //                        Text("\(item.0)")
@@ -56,7 +62,8 @@ struct DetailView: View {
             let (data, _) = try await URLSession.shared.data(from: url)
             if let decodedResponse = try? JSONDecoder().decode([String:[[String: String?]]].self, from: data) {
                 let dict = (decodedResponse["meals"]?[0])!
-                recipe.instructions = dict["strInstructions"]!!
+                recipe.instructions = dict["strInstructions"]!!.replacingOccurrences(of: "\r\n", with: "").components(separatedBy: ".")
+                print(recipe.instructions)
                 recipe.ingredients = dict.filter({$0.key.hasPrefix("strIngredient") && $0.value != "" && $0.value != nil}).sorted(by: { Int($0.key.dropFirst(13))! < Int($1.key.dropFirst(13))!}).map({$0.value!})
                 recipe.measurements = dict.filter({$0.key.hasPrefix("strMeasure") && $0.value != " " && $0.value != nil}).sorted(by: { Int($0.key.dropFirst(10))! < Int($1.key.dropFirst(10))!}).map({$0.value!})
                 for i in 0..<recipe.ingredients.count {
